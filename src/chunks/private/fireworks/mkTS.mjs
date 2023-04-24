dd70e4754db9a7051166413a80480c586a872507
David Evans <davidje13@users.noreply.github.com>
2023-04-24 21:44:37 +0100

    Start parsing fireworks data

--- src/chunks/private/fireworks/mkTS.mjs
+++ src/chunks/private/fireworks/mkTS.mjs
@@ -1,11 +1,11 @@
 import { inflateSync } from 'node:zlib';
 import { registerChunk } from '../../registry.mjs';
-import { readNested } from './structure.mjs';
+import { readNested, simplifyNested } from './structure.mjs';
 
 registerChunk('mkTS', {}, (chunk, state, warnings) => {
   try {
     const inflated = inflateSync(chunk.data);
-    chunk.root = readNested(inflated, warnings);
+    chunk.root = simplifyNested(readNested(inflated, warnings));
     // TODO
   } catch (e) {
     warnings.push(`mkTS compressed data is unreadable ${e}`);
--- src/chunks/private/fireworks/structure.mjs
+++ src/chunks/private/fireworks/structure.mjs
@@ -9,6 +9,13 @@
 // s = string (2-byte big-endian number-of-characters, then text encoded as utf16-BE)
 // v = vector (contains other nodes)
 
+// some details seem to be documented here:
+// https://help.adobe.com/archive/en_US/fireworks/cs5/fireworks_cs5_extending.pdf
+// (e.g. EffectMoaID values)
+
+// TID seems to reference tiles from mkBT
+// TIL combines TID tiles
+
 export function readNested(buf, warnings) {
   const root = [];
   const stack = [];
@@ -80,5 +87,41 @@ export function readNested(buf, warnings) {
   if (stack.length) {
     warnings.push('mkBS missing }');
   }
-  return root;
+  return { name: '', value: root };
+}
+
+export function simplifyNested(doc) {
+  if (!Array.isArray(doc.value)) {
+    return doc;
+  }
+  const simplified = doc.value.map(simplifyNested);
+  const findChild = (name) => simplified.find((o) => o.name === name);
+  const findChildren = (name) => simplified.filter((o) => o.name === name);
+  switch (doc.name) {
+    case 'DCE':
+      if (simplified.length !== 2) {
+        return doc;
+      }
+      return {
+        name: 'DCE-flat',
+        key: findChild('DCK')?.value,
+        value: findChild('DCV')?.value,
+      };
+    case 'EPS': {
+      const dces = findChildren('DCE-flat');
+      if (dces.length !== simplified.length) {
+        return doc;
+      }
+      const data = {};
+      for (const dce of dces) {
+        Object.defineProperty(data, dce.key, { value: dce.value, enumerable: true });
+      }
+      return {
+        name: 'EPS-flat',
+        data,
+      };
+    }
+    default:
+      return { name: doc.name, value: simplified };
+  }
 }
