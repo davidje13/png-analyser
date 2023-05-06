cf177deb05c51d70f4b577aa111c7fac6a6113a1
David Evans <davidje13@users.noreply.github.com>
2023-05-06 11:23:41 +0100

    Add missing patterns file

---
+++ src/chunks/private/fireworks/tree/nodes/patterns.mjs
@@ -0,0 +1,43 @@
+import { getBasicValue, getChild, registerNode } from '../node_registry.mjs';
+import { outputNodes } from './generic.mjs';
+
+registerNode('TXB', 'v', {
+  read: (target, value) => {
+    const name = getBasicValue(value, 'INM', 's');
+    const mask = getChild(value, 'MSK', 'v');
+
+    Object.assign(target, outputNodes('Brush texture: ' + JSON.stringify(name), [mask]));
+  },
+});
+
+registerNode('TXF', 'v', {
+  read: (target, value) => {
+    const name = getBasicValue(value, 'INM', 's');
+    const mask = getChild(value, 'MSK', 'v');
+
+    Object.assign(target, outputNodes('Fill texture: ' + JSON.stringify(name), [mask]));
+  },
+});
+
+registerNode('PAT', 'v', { // PATtern (?)
+  read: (target, value) => {
+    const fill = getChild(value, 'FPL', 'v');
+    const fillCol = getChild(value, 'FCL', 'i');
+    const fillTex = getChild(value, 'TXF', 'v');
+
+    const brush = getChild(value, 'BPL', 'v');
+    const brushCol = getChild(value, 'BCL', 'i');
+    const brushTex = getChild(value, 'TXB', 'v');
+
+    target.value = value;
+
+    Object.assign(target, outputNodes('Pattern', [
+      fill,
+      fillCol,
+      fill?.usesTexture ? fillTex : undefined,
+      brush,
+      brushCol,
+      brush?.usesTexture ? brushTex : undefined,
+    ]));
+  },
+});
