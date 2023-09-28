ab8e48612a2df15f9975dc17c47b62b2b37dc55d
David Evans <davidje13@users.noreply.github.com>
2023-09-28 01:36:56 +0100

    Apply masks to 24-bit images in icns files

--- src/image/icns/icns.mjs
+++ src/image/icns/icns.mjs
@@ -14,8 +14,9 @@ import { isJPEG, isJPEG2000 } from '../jpeg/jpeg.mjs';
  * }} IconSizeOut
  *
  * @typedef {{ images: IconSizeOut[], sub: SubIcons[], name?: string, version?: number }} State
+ * @typedef {Omit<State, 'images'> & { images: (IconSizeOut & { type: number; maskType: number | null; used?: boolean })[] }} InnerState
  * @typedef {{ type: string, state: State }} SubIcons
- * @typedef {(state: State, data: DataView, warnings: string[]) => void} ChunkHandler
+ * @typedef {(state: InnerState, data: DataView, type: number, warnings: string[]) => void} ChunkHandler
  * @typedef {{ handler: ChunkHandler }} Chunk
  */
 
@@ -56,7 +57,7 @@ export function readICNS(data, { expectHeader = true } = {}) {
     warnings.push(`file size ${dv.byteLength} does not match expected size ${size}`);
   }
 
-  /** @type {State} */ const state = { images: [], sub: [] };
+  /** @type {InnerState} */ const state = { images: [], sub: [] };
 
   let p = hasHeader ? 8 : 0;
   while (p < size) {
@@ -72,10 +73,33 @@ export function readICNS(data, { expectHeader = true } = {}) {
     const mode = CHUNKS.get(iconType);
     if (mode) {
       /** @type {string[]} */ const w = [];
-      mode.handler(state, iconData, w);
+      mode.handler(state, iconData, iconType, w);
       warnings.push(...w.map((warning) => `${printTag(iconType)}: ${warning}`));
     } else {
       warnings.push(`unknown icon type: ${printTag(iconType)}`);
+    }
+  }
+
+  for (const image of state.images) {
+    if (image.maskType !== null) {
+      const mask = state.images.find((i) => i.type === image.maskType);
+      if (mask) {
+        const w = image.image[0].length;
+        const h = image.image.length;
+        for (let y = 0; y < h; ++y) {
+          for (let x = 0; x < w; ++x) {
+            image.image[y][x] &= mask.image[y][x];
+          }
+        }
+        mask.used = true;
+      }
+    }
+  }
+  for (let i = 0; i < state.images.length;) {
+    if (state.images[i].used) {
+      state.images.splice(i, 1);
+    } else {
+      ++i;
     }
   }
 
@@ -94,11 +118,11 @@ function registerChunk(tag, handler) {
   CHUNKS.set(tag, { handler });
 }
 
-registerChunk(char32('TOC '), (state, data, warnings) => {
+registerChunk(char32('TOC '), (state, data, type, warnings) => {
   // Table of Contents
 });
 
-registerChunk(char32('icnV'), (state, data, warnings) => {
+registerChunk(char32('icnV'), (state, data, type, warnings) => {
   // tooling version number
   if (data.byteLength !== 4) {
     warnings.push(`expected 4 bytes, got ${data.byteLength}`);
@@ -106,23 +130,23 @@ registerChunk(char32('icnV'), (state, data, warnings) => {
   state.version = data.getFloat32(0);
 });
 
-registerChunk(char32('name'), (state, data, warnings) => {
+registerChunk(char32('name'), (state, data, type, warnings) => {
   // 'icon' or 'template'
   state.name = getLatin1(data);
 });
 
-registerChunk(char32('info'), (state, data, warnings) => {
+registerChunk(char32('info'), (state, data, type, warnings) => {
   // info binary plist with name property
 });
 
 /**
- * @param {string} type
+ * @param {string} identifier
  * @return {ChunkHandler}
  */
-const SUBSET_HANDLER = (type) => (state, data, warnings) => {
+const SUBSET_HANDLER = (identifier) => (state, data, type, warnings) => {
   const sub = readICNS(data, { expectHeader: false });
-  warnings.push(...sub.warnings.map((warning) => `${type} sub-icon: ${warning}`));
-  state.sub.push({ type, state: sub.state });
+  warnings.push(...sub.warnings.map((warning) => `${identifier} sub-icon: ${warning}`));
+  state.sub.push({ type: identifier, state: sub.state });
 };
 
 registerChunk(char32('sbtp'), SUBSET_HANDLER('template'));
@@ -137,13 +161,14 @@ registerChunk(0xFDD92FA8, SUBSET_HANDLER('dark'));
  * @param {number=} options.scale
  * @param {number=} options.bits
  * @param {number=} options.mask
+ * @param {number | null=} options.maskRef
  * @param {boolean=} options.prefix4Zeros
  * @param {boolean | 'unreliable'=} options.jpeg
  * @param {boolean | 'unreliable'=} options.png
  *
  * @return {ChunkHandler}
  */
-const ICON_HANDLER = ({ size = 0, width = size, height = size, scale = 1, bits = 0, mask = 0, prefix4Zeros = false, jpeg = false, png = false }) => (state, data, warnings) => {
+const ICON_HANDLER = ({ size = 0, width = size, height = size, scale = 1, bits = 0, mask = 0, maskRef = null, prefix4Zeros = false, jpeg = false, png = false }) => (state, data, type, warnings) => {
   if ((png || jpeg || prefix4Zeros) && isPNG(data)) {
     if (!png) {
       warnings.push('unexpected PNG data');
@@ -157,7 +182,7 @@ const ICON_HANDLER = ({ size = 0, width = size, height = size, scale = 1, bits =
       if (image.length !== height || image[0]?.length !== width) {
         warnings.push(`PNG has unexpected size: expected ${width}x${height}, got ${image[0]?.length ?? 0}x${image.length}`);
       }
-      state.images.push({ bitDepth: 32, scale, image, rawPNG: p });
+      state.images.push({ type, maskType: null, bitDepth: 32, scale, image, rawPNG: p });
     }
     return;
   }
@@ -186,6 +211,12 @@ const ICON_HANDLER = ({ size = 0, width = size, height = size, scale = 1, bits =
   }
 
   if (bits === 32) { // ARGB
+    const prefix = data.getUint32(0);
+    if (prefix === char32('ARGB')) {
+      data = subViewFrom(data, 4);
+    } else {
+      warnings.push(`expected ARGB data to begin with 'ARGB', got ${hex32(prefix)}`);
+    }
     const uncompressed = unpack(data, s * 4, warnings);
     for (let y = 0; y < height; ++y) {
       /** @type {number[]} */ const row = [];
@@ -221,14 +252,14 @@ const ICON_HANDLER = ({ size = 0, width = size, height = size, scale = 1, bits =
       /** @type {number[]} */ const row = [];
       for (let x = 0; x < width; ++x) {
         const p = y * width + x;
-        const c = paletteIndices ? palette[paletteIndices[p]] & 0xFFFFFF : 0;
+        const c = paletteIndices ? palette[paletteIndices[p]] & 0xFFFFFF : 0xFFFFFF;
         const a = alpha ? alphaConv[alpha[p]] : 0xFF000000;
         row.push((a | c) >>> 0);
       }
       image.push(row);
     }
   }
-  state.images.push({ bitDepth: bits, scale, image });
+  state.images.push({ type, maskType: maskRef, bitDepth: bits, scale, image });
 };
 
 const PALETTES = new Map([
@@ -304,41 +335,50 @@ function readBitArray(data, count, bits, warnings) {
   return result;
 }
 
-registerChunk(char32('ICON'), ICON_HANDLER({ size: 32, bits: 1 }));
-registerChunk(char32('ICN#'), ICON_HANDLER({ size: 32, bits: 1, mask: 1 }));
 registerChunk(char32('icm#'), ICON_HANDLER({ width: 16, height: 12, bits: 1, mask: 1 }));
 registerChunk(char32('icm4'), ICON_HANDLER({ width: 16, height: 12, bits: 4 }));
 registerChunk(char32('icm8'), ICON_HANDLER({ width: 16, height: 12, bits: 8 }));
+
 registerChunk(char32('ics#'), ICON_HANDLER({ size: 16, bits: 1, mask: 1 }));
 registerChunk(char32('ics4'), ICON_HANDLER({ size: 16, bits: 4 }));
 registerChunk(char32('ics8'), ICON_HANDLER({ size: 16, bits: 8 }));
-registerChunk(char32('is32'), ICON_HANDLER({ size: 16, bits: 24 }));
+registerChunk(char32('is32'), ICON_HANDLER({ size: 16, bits: 24, maskRef: char32('s8mk') }));
+registerChunk(char32('icp4'), ICON_HANDLER({ size: 16, bits: 24, jpeg: 'unreliable', png: 'unreliable', maskRef: char32('s8mk') }));
+registerChunk(char32('ic04'), ICON_HANDLER({ size: 16, bits: 32, jpeg: 'unreliable', png: 'unreliable' }));
 registerChunk(char32('s8mk'), ICON_HANDLER({ size: 16, mask: 8 }));
+
+registerChunk(char32('icsb'), ICON_HANDLER({ size: 18, bits: 32, jpeg: 'unreliable', png: 'unreliable' }));
+
+registerChunk(char32('sb24'), ICON_HANDLER({ size: 24, jpeg: true, png: true }));
+
+registerChunk(char32('ICON'), ICON_HANDLER({ size: 32, bits: 1 }));
+registerChunk(char32('ICN#'), ICON_HANDLER({ size: 32, bits: 1, mask: 1 }));
 registerChunk(char32('icl4'), ICON_HANDLER({ size: 32, bits: 4 }));
 registerChunk(char32('icl8'), ICON_HANDLER({ size: 32, bits: 8 }));
-registerChunk(char32('il32'), ICON_HANDLER({ size: 32, bits: 24 }));
+registerChunk(char32('il32'), ICON_HANDLER({ size: 32, bits: 24, maskRef: char32('l8mk') }));
+registerChunk(char32('icp5'), ICON_HANDLER({ size: 32, bits: 24, jpeg: 'unreliable', png: 'unreliable', maskRef: char32('l8mk') }));
 registerChunk(char32('l8mk'), ICON_HANDLER({ size: 32, mask: 8 }));
+
 registerChunk(char32('ich#'), ICON_HANDLER({ size: 48, bits: 1, mask: 1 }));
 registerChunk(char32('ich4'), ICON_HANDLER({ size: 48, bits: 4 }));
 registerChunk(char32('ich8'), ICON_HANDLER({ size: 48, bits: 8 }));
-registerChunk(char32('ih32'), ICON_HANDLER({ size: 48, bits: 24 }));
-registerChunk(char32('h8mk'), ICON_HANDLER({ size: 48, mask: 8 }));
-registerChunk(char32('it32'), ICON_HANDLER({ size: 128, bits: 24, prefix4Zeros: true }));
-registerChunk(char32('t8mk'), ICON_HANDLER({ size: 128, mask: 8 }));
-registerChunk(char32('icp4'), ICON_HANDLER({ size: 16, bits: 24, jpeg: 'unreliable', png: 'unreliable' }));
-registerChunk(char32('icp5'), ICON_HANDLER({ size: 32, bits: 24, jpeg: 'unreliable', png: 'unreliable' }));
+registerChunk(char32('ih32'), ICON_HANDLER({ size: 48, bits: 24, maskRef: char32('h8mk') }));
 registerChunk(char32('icp6'), ICON_HANDLER({ size: 48, jpeg: 'unreliable', png: 'unreliable' }));
+registerChunk(char32('h8mk'), ICON_HANDLER({ size: 48, mask: 8 }));
+
+registerChunk(char32('it32'), ICON_HANDLER({ size: 128, bits: 24, prefix4Zeros: true, maskRef: char32('t8mk')}));
 registerChunk(char32('ic07'), ICON_HANDLER({ size: 128, jpeg: true, png: true }));
+registerChunk(char32('t8mk'), ICON_HANDLER({ size: 128, mask: 8 }));
+
 registerChunk(char32('ic08'), ICON_HANDLER({ size: 256, jpeg: true, png: true }));
+
 registerChunk(char32('ic09'), ICON_HANDLER({ size: 512, jpeg: true, png: true }));
-registerChunk(char32('ic10'), ICON_HANDLER({ size: 1024, scale: 2, jpeg: true, png: true }));
+
 registerChunk(char32('ic11'), ICON_HANDLER({ size: 32, scale: 2, jpeg: true, png: true }));
+registerChunk(char32('ic05'), ICON_HANDLER({ size: 32, scale: 2, bits: 32, jpeg: 'unreliable', png: 'unreliable' }));
+registerChunk(char32('icsB'), ICON_HANDLER({ size: 36, scale: 2, jpeg: true, png: true }));
+registerChunk(char32('SB24'), ICON_HANDLER({ size: 48, scale: 2, jpeg: true, png: true }));
 registerChunk(char32('ic12'), ICON_HANDLER({ size: 64, scale: 2, jpeg: true, png: true }));
 registerChunk(char32('ic13'), ICON_HANDLER({ size: 256, scale: 2, jpeg: true, png: true }));
 registerChunk(char32('ic14'), ICON_HANDLER({ size: 512, scale: 2, jpeg: true, png: true }));
-registerChunk(char32('ic04'), ICON_HANDLER({ size: 16, bits: 32, jpeg: 'unreliable', png: 'unreliable' }));
-registerChunk(char32('ic05'), ICON_HANDLER({ size: 32, scale: 2, bits: 32, jpeg: 'unreliable', png: 'unreliable' }));
-registerChunk(char32('icsb'), ICON_HANDLER({ size: 18, bits: 32, jpeg: 'unreliable', png: 'unreliable' }));
-registerChunk(char32('icsB'), ICON_HANDLER({ size: 36, scale: 2, jpeg: true, png: true }));
-registerChunk(char32('sb24'), ICON_HANDLER({ size: 24, jpeg: true, png: true }));
-registerChunk(char32('SB24'), ICON_HANDLER({ size: 48, scale: 2, jpeg: true, png: true }));
+registerChunk(char32('ic10'), ICON_HANDLER({ size: 1024, scale: 2, jpeg: true, png: true }));
